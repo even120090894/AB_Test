e06113448e18bfb1f96ad7cebbd5fd7a750fa918
even1128 <120090894@link.cuhk.edu.cn>
2025-01-05 01:08:45 +0800

    使用buffer而非texture记录卷积核信息
    texture会做一次1-255的颜色转化，转成texture后只保留整数。会有信息缺失。

--- Assets/Script/Lenia/LeniaManager.cs
+++ Assets/Script/Lenia/LeniaManager.cs
@@ -99,7 +99,7 @@ public class LeniaManager : MonoBehaviour
         int kernelIndex = kernelIndexDict[KernelFunction.WorldUpdate];
         leniaShader.SetTexture(kernelIndex, "oldWorldTexture", oldWorldT);
         leniaShader.SetTexture(kernelIndex, "newWorldTexture", newWorldT);
-    
+        leniaShader.SetBuffer(kernelIndex, "KernelCoreBuffer", kernelCoreBuffer);
 
         leniaImage.material = leniaMaterial;
         leniaImage.texture = oldWorldT;
@@ -117,8 +117,6 @@ public class LeniaManager : MonoBehaviour
         ableUpdate = false;
 
         int kernelIndex = kernelIndexDict[KernelFunction.WorldUpdate];
-        // leniaShader.SetTexture(kernelIndex, "oldWorldTexture", oldWorldT);
-        // leniaShader.SetTexture(kernelIndex, "newWorldTexture", newWorldT);
         leniaShader.Dispatch(kernelIndex, leniaWidth / 8, leniaWidth / 8, 1);
 
         // 使用AsyncGPUReadback确保GPU计算完成
@@ -134,20 +132,15 @@ public class LeniaManager : MonoBehaviour
             return;
         }
         // 读取RenderTexture的数据
-        var data = request.GetData<Color32>();
+        // var data = request.GetData<Color32>();
 
         // 将所有R通道值加起来
-        float totalR = 0;
-        for (int i = 0; i < data.Length; i++)
-        {
-            totalR += data[i].r;
-            // if (data[i].r != 0)
-            // {
-            //     Debug.Log("R value !!!!: "+i + " : " + data[i].r);
-            // }
-        }
-        Debug.Log("Total R value: " + totalR / 256 / 256);
-
+        // float totalR = 0;
+        // for (int i = 0; i < data.Length; i++)
+        // {
+        //     totalR += data[i].r;
+        // }
+        // Debug.Log("Total R value: " + totalR / 256 / 256);
 
         Graphics.Blit(newWorldT, oldWorldT);
         ableUpdate = true;
@@ -201,24 +194,43 @@ public class LeniaManager : MonoBehaviour
         var data = request.GetData<Color32>();
 
         // 将所有R通道值加起来
-        float totalR = 0;
-        for (int i = 0; i < data.Length; i++)
-        {
-            totalR += data[i].r;
-        }
-        Debug.Log("Total R value: " + totalR);
+        // float totalR = 0;
+        // for (int i = 0; i < data.Length; i++)
+        // {
+        //     totalR += data[i].r;
+        // }
+        // Debug.Log("Total R value: " + totalR);
+        
+        // 读取compute buffer中的数据
+        float totalC = ProcessBufferData();
 
-        KernelNormalize(totalR);
+        KernelNormalize(totalC);
+    }
+    private float ProcessBufferData()
+    {
+        // 同步获取ComputeBuffer的数据
+        int kernelLength = Mathf.CeilToInt(2.0f * max_conv_r / 8.0f);
+        float[] bufferData = new float[kernelLength * kernelLength * 8 * 8]; // 和你的buffer大小一致
+        kernelCoreBuffer.GetData(bufferData);  // 获取数据
+        // 处理你获取到的buffer数据
+        float totalBufferValue = 0;
+        for (int i = 0; i < bufferData.Length; i++)
+        {
+            totalBufferValue += bufferData[i];
+        }
+        Debug.Log("Total Buffer value: " + totalBufferValue); // 此处数值应该是Total R数值的 1/255，因为texture数值会做一次rgb值的转化
+        return totalBufferValue;
     }
 
-    private void KernelNormalize(float totalR)
+    private void KernelNormalize(float totalC)
     {
         int kernelLengthNum = Mathf.CeilToInt(2.0f * max_conv_r / 8.0f);
         int kernelIndex = kernelIndexDict[KernelFunction.KernelNormalize];
 
-        leniaShader.SetFloat("accumulatedResult", totalR);
+        leniaShader.SetFloat("accumulatedResult", totalC);
 
         leniaShader.SetTexture(kernelIndex, "KernelCoreResult", kernelCoreTexture);
+        leniaShader.SetBuffer(kernelIndex, "KernelCoreBuffer", kernelCoreBuffer);
         leniaShader.Dispatch(kernelIndex, kernelLengthNum, kernelLengthNum, 1);
 
         AsyncGPUReadback.Request(kernelCoreTexture, 0, TextureFormat.RGBA32, OnKernelNormalizeReadback);
@@ -235,14 +247,16 @@ public class LeniaManager : MonoBehaviour
         // 读取RenderTexture的数据
         var data = request.GetData<Color32>();
         // 将所有R通道值加起来
-        float totalR = 0;
-        for (int i = 0; i < data.Length; i++)
-        {
-            totalR += data[i].r;
-        }
-        Debug.Log("Normalize R value: " + totalR);
+        // float totalR = 0;
+        // for (int i = 0; i < data.Length; i++)
+        // {
+        //     totalR += data[i].r;
+        // }
+        // Debug.Log("Normalize R value: " + totalR);
 
-        // WorldInit();
+        ProcessBufferData();
+
+        WorldInit();
     }
 
 
